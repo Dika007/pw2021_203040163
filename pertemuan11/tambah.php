8a6168b810fd08ba4eae3086a343891409699a7f
Dika007 <dsa799059@gmail.com>
2021-05-02 01:32:43 +0700

    menyelesaikan pertemuan11

---
+++ pertemuan11/tambah.php
@@ -0,0 +1,112 @@
+<?php
+require 'functions.php';
+
+//cek apakah tombol tambah sudah ditekan
+if (isset($_POST['tambah'])) {
+
+
+  if (tambah($_POST) > 0) {
+    echo "<script>
+              alert('data berhasil di tambahkan');
+              document.location.href = 'index.php';
+            </script>";
+  } else {
+    echo "data gagal ditambahkan";
+  }
+}
+
+?>
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tambah data mahasiswa</title>
+</head>
+
+<body>
+  <h3>form tambah data mahasiswa</h3>
+
+  <form action="" method="POST">
+    <ul>
+
+      <li>
+        <label>
+
+          nama :
+          <input type="text" name="nama" autofocus required>
+        </label>
+
+      </li>
+
+
+      <li>
+        <label>
+
+          nrp :
+          <input type="text" name="nrp" required>
+        </label>
+
+      </li>
+
+
+      <li>
+        <label>
+
+          email :
+          <input type="text" name="email" required>
+        </label>
+
+      </li>
+
+
+      <li>
+        <label>
+
+          jurusan :
+          <input type="text" name="jurusan" required>
+        </label>
+
+      </li>
+
+
+      <li>
+        <label>
+
+          gambar :
+          <input type="text" name="gambar" required>
+        </label>
+
+      </li>
+
+      <li>
+        <button type="submit" name="tambah">tambah data!!</button>
+      </li>
+
+    </ul>
+
+
+
+
+
+
+  </form>
+
+
+
+
+
+</body>
+
+</html>
---
+++ pertemuan11/ubah.php
@@ -0,0 +1,125 @@
+<?php
+require 'functions.php';
+
+// jika tidak ada id di url 
+if (!isset($_GET['id'])) {
+  header("Location: index.php");
+  exit;
+}
+
+// ambil id dari url
+$id = $_GET['id'];
+
+// query mahasiswa berdasarkan id
+$m = query("SELECT * FROM mahasiswa WHERE id = $id");
+
+//cek apakah tombol ubah sudah ditekan
+if (isset($_POST['ubah'])) {
+
+
+  if (ubah($_POST) > 0) {
+    echo "<script>
+              alert('data berhasil di ubah');
+              document.location.href = 'index.php';
+            </script>";
+  } else {
+    echo "data gagal di ubah";
+  }
+}
+
+?>
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ubah data mahasiswa</title>
+</head>
+
+<body>
+  <h3>Form ubah data mahasiswa</h3>
+
+  <form action="" method="POST">
+    <input type="hidden" name="id" value="<?= $m['id']; ?>">
+    <ul>
+
+      <li>
+        <label>
+
+          nama :
+          <input type="text" name="nama" autofocus required value="<?= $m['nama']; ?>">
+        </label>
+
+      </li>
+
+
+      <li>
+        <label>
+
+          nrp :
+          <input type="text" name="nrp" required value="<?= $m['nrp']; ?>">
+        </label>
+
+      </li>
+
+
+      <li>
+        <label>
+
+          email :
+          <input type="text" name="email" required value="<?= $m['email']; ?>">
+        </label>
+
+      </li>
+
+
+      <li>
+        <label>
+
+          jurusan :
+          <input type="text" name="jurusan" required value="<?= $m['jurusan']; ?>">
+        </label>
+
+      </li>
+
+
+      <li>
+        <label>
+
+          gambar :
+          <input type="text" name="gambar" required value="<?= $m['gambar']; ?>">
+        </label>
+
+      </li>
+
+      <li>
+        <button type="submit" name="ubah">ubah data!!</button>
+      </li>
+
+    </ul>
+
+
+
+
+
+
+  </form>
+
+
+
+
+
+</body>
+
+</html>
